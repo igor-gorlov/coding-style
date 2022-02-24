3961d3303611313586f298e07111bd579234f274
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-02-24 19:04:39 +0300

    mod snp-prep: rewrite to support loading multiline snippets from files
    The implimentation does NOT work with local files — only via the web.

--- prepare_snippets.js
+++ prepare_snippets.js
@@ -1,3 +1,30 @@
-// Configure and run highlight.js.
-hljs.configure({cssSelector: "code.snippet"});
-hljs.highlightAll();
+// Load multiline snippets and highlight them.
+let placeholders = document.querySelectorAll("code.snippet[data-src]");
+for (let i of placeholders)
+{
+	let sourceURL = i.getAttribute("data-src");
+	let result = fetch(sourceURL)
+		.then(response => response.text())
+		.then(text =>
+			{
+				// If the placeholder is not a child of a <pre> element, create the latter and set it as a parent for the former.
+				if(!i.matches("pre>code"))
+				{
+					let wrapper = document.createElement("pre");
+					i.parentElement?.appendChild(wrapper);
+					wrapper.appendChild(i);
+				}
+				// Insert the content.
+				i.innerHTML = text;
+				// Highlight the snippet.
+				hljs.highlightElement(i);
+			}
+		);
+}
+
+// Highlight single-line snippets
+let snippets = document.querySelectorAll("code.snippet:not([data-src])");
+for(let i of snippets)
+{
+	hljs.highlightElement(i);
+}
