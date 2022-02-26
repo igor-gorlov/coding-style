674fa6fc8b0104be3fa647da9e9d6d7febb7867e
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-02-26 12:50:02 +0300

    feat snp-prep: prepend usage instructions

--- prepare_snippets.js
+++ prepare_snippets.js
@@ -1,3 +1,15 @@
+/*
+This script performs automatic loading and highlighting of source code fragments which are to be shown on a webpage.
+
+You can either put snippets directly into an HTML document or load them from files.
+In the former case, you have to escape special characters with HTML entities and enclose the code into <code>...</code>.
+Assign .snippet and .language-xxxx classes to the <code> tag, otherwise there is a threat of incorrect highlighting.
+The latter method uses an empty <code>...</code> pair with the same set of assigned classes as above; however, a
+data-src attribute is required to specify URL of the source file where the code fragment is stored. Don't worry about
+special characters - the script will escape them for you.
+*/
+
+
 // Load multiline snippets and highlight them.
 let placeholders = document.querySelectorAll("code.snippet[data-src]");
 for (let i of placeholders)
