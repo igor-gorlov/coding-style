6a420dc069903ed994a20463bb67409904cbc012
Igor Gorlov <gorlov-igorek@yandex.ru>
2022-05-11 13:29:17 +0300

    format: reapply Prettier

--- prepare_snippets.js
+++ prepare_snippets.js
@@ -16,37 +16,29 @@ following into your <head> container:
 	<script defer src="path/to/prepare_snippets.js"></script>
 */
 
-
 // Load multiline snippets and highlight them.
 let placeholders = document.querySelectorAll("code.snippet[data-src]");
-for (let i of placeholders)
-{
+for (let i of placeholders) {
 	let sourceURL = i.getAttribute("data-src");
 	let result = fetch(sourceURL)
 		.then(response => response.text())
-		.then(text =>
-			{
-				// If the placeholder is not a child of a <pre> element, create the latter and set it as a parent for the former.
-				if(!i.matches("pre>code"))
-				{
-					let wrapper = document.createElement("pre");
-					i.parentElement?.appendChild(wrapper);
-					wrapper.appendChild(i);
-				}
-				// Escape and insert the code.
-				let escapedCode = text.replace(/\</g, "&lt;")
-					.replace(/\>/g, "&gt;")
-					.replace(/\"/g, "&quot;");
-				i.innerHTML = escapedCode;
-				// Highlight the snippet.
-				hljs.highlightElement(i);
+		.then(text => {
+			// If the placeholder is not a child of a <pre> element, create the latter and set it as a parent for the former.
+			if (!i.matches("pre>code")) {
+				let wrapper = document.createElement("pre");
+				i.parentElement?.appendChild(wrapper);
+				wrapper.appendChild(i);
 			}
-		);
+			// Escape and insert the code.
+			let escapedCode = text.replace(/\</g, "&lt;").replace(/\>/g, "&gt;").replace(/\"/g, "&quot;");
+			i.innerHTML = escapedCode;
+			// Highlight the snippet.
+			hljs.highlightElement(i);
+		});
 }
 
 // Highlight single-line snippets
 let snippets = document.querySelectorAll("code.snippet:not([data-src])");
-for(let i of snippets)
-{
+for (let i of snippets) {
 	hljs.highlightElement(i);
 }
